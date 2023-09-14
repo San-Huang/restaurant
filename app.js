0e4433ac90ce70126f337945615d8e240bd8b76f
San-Huang <sanyeha7@gmail.com>
2023-09-14 23:06:10 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -4,32 +4,33 @@ const restaurantsData = require('./public/jsons/restaurant.json').results
 const app = express()
 const port = 3000
 
-app.engine('.hbs', engine({extname: '.hbs'})) //告訴express我要用handler
-app.set('view engine', '.hbs')
-app.use(express.static('public'))
+// app.METHOD(PATH,HANDLER) path是endpoint；handler是一個function，一旦伺服器在app.listen(指定的port收到method與path的請求，就會執行handler function來回應請求。
 
-app.get('/restaurantList', (req, res) => {
+app.engine('.hbs', engine({extname: '.hbs'})) //engine就是告訴express我要用樣板引擎handlebars
+app.set('view engine', '.hbs') //set?
+app.set('views', './views')
+app.use(express.static('public')) //use
+
+app.get('/', (req, res) => {
     res.render('index', { restaurantsData }) //將restaurant.json的資料丟進index
   })
 
 
-app.get("/search", (req, res) => {
+  app.get("/search", (req, res) => {
     const keywords = req.query.keywords
     const keyword = req.query.keywords.trim().toLowerCase()
-    
     if (!req.query.keywords) {
       return res.redirect("/")
     }
-  
+
     const filterRestaurantsData = restaurantsData.filter(
-      data =>
+      (data) =>
         data.name.toLowerCase().includes(keyword) ||
         data.category.includes(keyword)
     )
   
     res.render("index", { restaurantsData: filterRestaurantsData, keywords })
   })
-
 
   app.get("/restaurants/:restaurantId", (req, res) => {
     const { restaurantId } = req.params
@@ -39,7 +40,7 @@ app.get("/search", (req, res) => {
     res.render("show", { restaurantData })
   })
 
-
+  //  app.listen(port,callbackFn)一旦伺服器開始監聽指定的port，callbackFn就會被執行。
   app.listen(port, () => {
     console.log(`Express server is running on http://localhost:${port}`)
   })
