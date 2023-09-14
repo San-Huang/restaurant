4745cfb3e03968dc3420b2055cc5ac930188a123
San-Huang <sanyeha7@gmail.com>
2023-09-14 18:50:35 +0800

    feat: add listing page

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
+const restaurantsData = require('./public/jsons/restaurant.json').results
 const app = express()
 const port = 3000
 
-app.engine('.hbs', engine({extname: '.hbs'}))
+app.engine('.hbs', engine({extname: '.hbs'})) //告訴express我要用handler
 app.set('view engine', '.hbs')
-app.set('views', './views')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.redirect('/restaurantList')
-})
-
 app.get('/restaurantList', (req, res) => {
-    res.render('index')
-  })
-  
-  app.listen(port, () => {
-    console.log(`express server is running on http://localhost:${port}`)
+    res.render('index', { restaurantsData }) //將restaurant.json的資料丟進index
   })
 
-  app.get('/restaurantList/:id', (req, res) => {
-    const id = req.params.id
-    res.send(`read restaurantList: ${id}`)
+
+app.get("/search", (req, res) => {
+    const keywords = req.query.keywords
+    const keyword = req.query.keywords.trim().toLowerCase()
+    
+    if (!req.query.keywords) {
+      return res.redirect("/")
+    }
+  
+    const filterRestaurantsData = restaurantsData.filter(
+      data =>
+        data.name.toLowerCase().includes(keyword) ||
+        data.category.includes(keyword)
+    )
+  
+    res.render("index", { restaurantsData: filterRestaurantsData, keywords })
+  })
+
+
+  app.get("/restaurants/:restaurantId", (req, res) => {
+    const { restaurantId } = req.params
+    const restaurantData = restaurantsData.find(
+      data => data.id === Number(restaurantId)
+    )
+    res.render("show", { restaurantData })
+  })
+
+
+  app.listen(port, () => {
+    console.log(`Express server is running on http://localhost:${port}`)
   })
