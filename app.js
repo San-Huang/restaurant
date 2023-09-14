c07b6f5522eb526ea75c32b29d851fb8f03b5378
San-Huang <sanyeha7@gmail.com>
2023-09-14 13:05:18 +0800

    build: add bootstrap static files for style

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
-const port = 300
+const port = 3000
 
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send('express app for restaurant list')
+    res.redirect('/restaurantList')
+})
+
+app.get('/restaurantList', (req, res) => {
+    res.render('index')
   })
   
   app.listen(port, () => {
